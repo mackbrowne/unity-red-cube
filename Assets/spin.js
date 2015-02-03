0e2702f58f620396496a0cc679c129fc91c83a9b
Mack Browne <mackbrowne@gmail.com>
2015-02-03 12:22:03 -0500

    first commit

---
+++ Assets/spin.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+    // Use this for initialization
+    function Start () {
+    }
+
+    // Update is called once per frame
+    function Update () {
+        // rotate at 90 degrees per second
+        transform.Rotate(Vector3.up * Time.deltaTime*90);
+    }
